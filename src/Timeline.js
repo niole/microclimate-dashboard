0306803bfcdc8271f7a0b24d655bbdb00eb502cc
niole <niolenelson@gmail.com>
2020-08-17 14:23:53 -0700

    Make data nice

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Tooltip, Line, CartesianGrid, XAxis, YAxis, LineChart } from 'recharts';
+import { zones } from './floorplans/AptE';
+
+const formatCreatedAt = createdAt => {
+  const date = new Date(createdAt);
+  return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+};
 
 const Timeline = ({
   events,
-}) => (
-<div>{events.map(({ key, value, createdAt }) => (
-  <div key={createdAt}>
-    {key}{value}{createdAt}
-  </div>
-))}</div>
-);
+}) => {
+  const formattedEvents = events.map(({ key, value, createdAt }) => ({
+      [key]: value,
+      createdAt: formatCreatedAt(createdAt),
+  }));
+  return (
+    <LineChart width={500} height={300} data={formattedEvents}>
+      <Tooltip />
+      <XAxis dataKey="createdAt"/>
+      <YAxis/>
+      <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+      {Object.values(zones).map(zone => (
+        <Line type="monotone" key={zone} dataKey={zone} stroke="#8884d8" />
+      ))}
+    </LineChart>
+  );
+};
 
 export default Timeline;

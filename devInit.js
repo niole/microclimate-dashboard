e367ae6c7628293780a71f80288a8bd4160c3a89
niole <niolenelson@gmail.com>
2020-08-21 08:25:59 -0700

    Make dev init more interesting, ignore 32 degree readings

--- devInit.js
+++ devInit.js
@@ -12,3 +12,30 @@ for (var i=0; i < 10; i++) {
         createdAt: Date.now() - (i*24*60*60*1000)
     });
 }
+
+for (i=0; i < 10; i++) {
+    climateDb.events.insert({
+        roomName: "apte-bedroom",
+        temperature: (10-i)*10,
+        floorPlanName: "apte",
+        createdAt: Date.now() - (i*24*60*60*1000) - 300000
+    });
+}
+
+for (i=0; i < 10; i++) {
+    climateDb.events.insert({
+        roomName: "apte-livingroom",
+        temperature: Math.floor(Math.random()*10),
+        floorPlanName: "apte",
+        createdAt: Date.now() - (i*24*60*60*1000) - 600000
+    });
+}
+
+for (i=0; i < 10; i++) {
+    climateDb.events.insert({
+        roomName: "apte-livingroom",
+        temperature: Math.floor(Math.random()*10),
+        floorPlanName: "otherfloorplan",
+        createdAt: Date.now() - (i*24*60*60*1000) - 600000
+    });
+}
